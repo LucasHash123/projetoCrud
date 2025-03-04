36e1378c9e82f4b737c373626802cfa902dc8bc9
Lucas Santos <lucas090691@gmail.com>
2025-03-03 23:55:44 -0300

    atualizacao

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class ButtonComponent {
 
+  @Input() textButton: string;
 }
